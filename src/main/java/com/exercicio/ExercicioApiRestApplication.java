0228b492a497af30374f1eba17aa30cd8373a7e8
Maria Ester Veiga <esterdcveiga@gmail.com>
2022-08-12 13:46:57 -0300

    criação do projeto

---
+++ src/main/java/com/exercicio/ExercicioApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioApiRestApplication.class, args);
+	}
+
+}
